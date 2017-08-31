de18dd97e5cdfbe6c03d8acf77c49464f1a0ea07
Laurent Leconte <laurent.leconte@moonshot-internet.com>
2017-08-31 23:08:21 +0200

    implementation of algorithm B and corrections to alg A

--- python/permutations.py
+++ python/permutations.py
@@ -3,15 +3,16 @@ To stick to the original text, a permutation is represented as a list of tuples 
 So for instance `[(1, 2, 3),(4,6)]` corresponds to (a b c)(d f) using Knuth's notation.
 """
 
-def permutation_product(perms):
-    """Implement Algorithm A of chapter 1.3.3.
-    Arg:
-        perms: a list of cycles to multiply
+def permutation_product_A(perms):
+    """Implement algorithm B of chapter 1.3.3
+
+    :param perms: the list of permutations (themselves a list of cycles) to multiply
+    :return: The product of the permutations in normalized form
     """
-    # Start by flattening the input to a list of cycles if necessary
     if len(perms) == 0:
         #input is the identity permutation - return id
         return []
+    # Start by flattening the input to a list of cycles if necessary
     cycles = flatten_permutations(perms)
     # First pass (step A1). Tag left parens and replace right parens by tagged first element of cycle.
     # Tagging is done by negating the input. Left parens are actually not tracked, as they don't come into play.
@@ -31,7 +32,7 @@ def permutation_product(perms):
                 idx += 1
                 if idx == n:
                     #all elements are tagged: we're done
-                    return result
+                    return sorted(result)
             # open new cycle, add current element and tag it.
             start = symbols[idx]
             current_cycle = [start]
@@ -56,8 +57,72 @@ def permutation_product(perms):
         else:
             #step A6 : close current cycle and add it to the result. If cycle length is one, ignore it
             if len(current_cycle) > 1:
-                result.append(tuple(current_cycle))
+                result.append(normalized_tuple(current_cycle))
             closed = True
+
+def permutation_product_B(perms):
+    """Implement algorithm B of chapter 1.3.3
+
+    :param perms: the list of permutations (themselves a list of cycles) to multiply
+    :return: The product of the permutations in normalized form
+    """
+    if len(perms) == 0:
+        # Input is the identity permutation - return id
+        return []
+    # Start by flattening the input to a list of cycles if necessary
+    cycles = flatten_permutations(perms)
+
+    # Step B1: find n and init T.
+    n = max([max(cycle) for cycle in cycles])
+    T = list(range(n+1))
+    # Step B2 : iterate over the input from right to left
+    for cycle in reversed(cycles):
+        # Step B2 - input is ')'
+        # Here, T[0] is used to hold Z
+        T[0] = 0
+        for i in reversed(cycle):
+            # Step B3 : switch Z and T[i]
+            T[0], T[i] = T[i], T[0]
+            if T[i] == 0:
+                j = i
+        # Step B4
+        T[j] = T[0]
+    # end of main algorithm. Now translate the result in cycle form.
+    result = []
+    T[0] = 0
+    while True:
+        # find first untagged element
+        idx = 0
+        while idx <= n and T[idx] <= 0:
+            idx += 1
+        if idx > n:
+            # no more untagged elements, return result
+            return sorted(result)
+        # start new cycle
+        cycle = []
+        while T[idx] > 0:
+            # add current element to cycle
+            cycle.append(idx)
+            # tag element and go to next one
+            j = T[idx]
+            T[idx] = 0
+            idx = j
+        # end of cycle, add it to result if it is not a singleton
+        if len(cycle) > 1:
+            result.append(normalized_tuple(cycle))
+
+def normalized_tuple(cycle):
+    """Return a normalized version (starting from the smallest element) of the cycle
+    :param cycle: The cycle (in list form) to normalize
+    :return: A tuple representing the cycle
+    """
+    min_val = cycle[0]
+    min_idx = 0
+    for idx in range(1, len(cycle)):
+        if cycle[idx] < min_val:
+            min_val = cycle[idx]
+            min_idx = idx
+    return tuple(cycle[min_idx:] + cycle[:min_idx])
 
 def flatten_permutations(perms):
     if isinstance(perms[0], list):
@@ -71,7 +136,8 @@ if __name__ == '__main__':
     perm2 = [(3,5),(1,4)]
     perms = [perm1, perm2]
     knuth = [(1, 3, 6, 7), (2, 3, 4), (1, 5, 4), (6, 1, 4, 5), (2, 7, 6, 1, 5)]
-    print(permutation_product(knuth))
-    print(permutation_product([(1,)]))
-    print(permutation_product(perm1 + inv_perm1))
+    print(permutation_product_B(knuth))
+    print(permutation_product_A(knuth))
+    print(permutation_product_B([(1,)]))
+    print(permutation_product_B(perm1 + inv_perm1))
 
--- python/tests.py
+++ python/tests.py
@@ -1,21 +1,31 @@
 import unittest
-from permutations import permutation_product
+from permutations import permutation_product_A, permutation_product_B
 
 class PremutationTest(unittest.TestCase):
 
     def setUp(self):
         #example from TAoCP
         self.knuth = [(1, 3, 6, 7), (2, 3, 4), (1, 5, 4), (6, 1, 4, 5), (2, 7, 6, 1, 5)]
-        self.knuth_res = [(1, 4, 7), (3, 5, 2)]
+        self.knuth_res = [(1, 4, 7), (2, 3, 5)]
         self.perm1 = [(1, 2, 3), (4, 6)]
-        self.inv_perm1 = [(4, 6), (2, 1, 3)]
+        self.inv_perm1 = [(4, 6), (1, 3, 2)]
 
-    def test_permutation_product(self):
+    def test_permutation_product_A(self):
         #identity -> nothing happens
-        self.assertEqual(permutation_product([]), [])
-        self.assertEqual(permutation_product([(1,)]), [])
+        self.assertEqual(permutation_product_A([]), [])
+        self.assertEqual(permutation_product_A([(1,)]), [])
         #example from TAoCP
-        self.assertEqual(permutation_product(self.knuth), self.knuth_res)
+        self.assertEqual(permutation_product_A(self.knuth), self.knuth_res)
         #product with an inverse should yield identity
-        self.assertEqual(permutation_product(self.perm1 + self.inv_perm1), [])
-        self.assertEqual(permutation_product([self.perm1, self.inv_perm1]), [])
+        self.assertEqual(permutation_product_A(self.perm1 + self.inv_perm1), [])
+        self.assertEqual(permutation_product_A([self.perm1, self.inv_perm1]), [])
+
+    def test_permutation_product_B(self):
+        #identity -> nothing happens
+        self.assertEqual(permutation_product_B([]), [])
+        self.assertEqual(permutation_product_B([(1,)]), [])
+        #example from TAoCP
+        self.assertEqual(permutation_product_B(self.knuth), self.knuth_res)
+        #product with an inverse should yield identity
+        self.assertEqual(permutation_product_B(self.perm1 + self.inv_perm1), [])
+        self.assertEqual(permutation_product_B([self.perm1, self.inv_perm1]), [])
