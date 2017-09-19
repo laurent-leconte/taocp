222d39150e313a9327b00494a0a6879b454057da
Laurent Leconte <laurent.leconte@moonshot-internet.com>
2017-09-19 19:16:33 +0200

    polynomial addition (2.2.4)

--- python/linkedlists.py
+++ python/linkedlists.py
@@ -1,52 +1,112 @@
-"""Implementation of the linked list algorithms (chapter 2.2.3), in particular the topological sort.
+"""Implementation of the linked list  and circular lists algorithms (chapter 2.2.3 and 2.2.4).
 
 """
 
+import itertools
+
 def topological_sort(relations):
-    """Implement algorithm T of chapter 2.2.3. In this implementation the linked lists are actually replaced by lists.
+    """Implement algorithm T of chapter 2.2.3.
+
+    In this implementation the linked lists are actually replaced by lists.
     This does not depart significantly from the spirit of the original as we only use the `pop` and `append` methods.
 
     :param relations: a list of pairs representing the relations between objects
     :return: the list of objects in sorted order
     :raises:
     """
-    #step T1 : initialize N, count and top
-    #as usual, we will keep the 1-indexing from Knuth and ignore the first value of the lists.
+    # step T1 : initialize N, count and top
+    # as usual, we will keep the 1-indexing from Knuth and ignore the first value of the lists.
     n = max([max(e) for e in relations])
     N = n
     count = [0]*(n+1)
     top = []
-    #Can't use top = [[]]*(n+1) or we would modify all elements every time we push to one of them (shallow copy)
+    # Can't use top = [[]]*(n+1) or we would modify all elements every time we push to one of them (shallow copy)
     for i in range(n+1):
         top.append([])
-    #step T2 / T3 : ingest relations
+    # step T2 / T3 : ingest relations
     for (j,k) in relations:
         count[k] += 1
         top[j].append(k)
-    #step T4
+    # step T4
     qlink = []
     for k in range(1, n+1):
         if count[k] == 0:
             qlink.append(k)
     result = []
-    #loop on queue
+    # loop on queue
     while len(qlink) > 0:
-        #Step T5 + T7
+        # Step T5 + T7
         f = qlink.pop()
         result.append(f)
         N -= 1
-        #T6 : erase relations
+        # T6 : erase relations
         while len(top[f]) > 0:
             suc = top[f].pop()
             count[suc] -= 1
             if count[suc] == 0:
                 qlink.append(suc)
-    #step T8
+    # step T8
     if N == 0:
         return result
     else:
         raise ValueError("Loop in input relations")
 
+
+def polynomial_addition(p, q):
+    """Implement algorithm A of chapter 2.2.4.
+
+    This algorithm does not actually make use of circular lists, as each list is traversed only once.
+    Nonetheless, the implementation uses the `cycle` method from itertools lists to emulate a circular list (this frees
+    us from the temptation to test for the end of the list to terminate the algorithm, and prepares us for the
+    multiplication algorithm).
+    Each polynomial is described as a list of nested tuples of the form (coef, (a, b, c)).
+    The last element of each polynomial is always (0, (0, 0, -1)).
+
+    :param p: the first polynomial to add
+    :param q: the second polynomial to add
+    :return: the sum of p and q as a new list (note that this is different from the original algorithm
+    which adds to q in place.)
+    """
+    result = []
+    circular_p = itertools.cycle(p)
+    circular_q = itertools.cycle(q)
+    # step A1. P and Q are used to denote the value of the current element of p and q
+    P = next(circular_p)
+    Q = next(circular_q)
+
+    def coef(X):
+        return X[0]
+
+    def abc(X):
+        return X[1]
+    # loop over p and q while the special node hasn't been reached
+    while True:
+        while abc(P) < abc(Q):
+            # step A2
+            result.append(Q)
+            Q = next(circular_q)
+        while abc(P) > abc(Q):
+            # step A5
+            result.append(P)
+            P = next(circular_p)
+        if abc(P) == abc(Q):
+            # step A3
+            if abc(P) == (0, 0, -1):
+                # exit condition of step A3. Use Q as short-hand for the special node
+                return result + [Q]
+            c = coef(P) + coef(Q)
+            if c != 0:
+                result.append((c, abc(P)))
+            P = next(circular_p)
+            Q = next(circular_q)
+
+
+
+
+
 if __name__ == '__main__':
     relations = [(9, 2), (3, 7), (7, 5), (5, 8), (8, 6), (4, 6), (1, 3), (7, 4), (9, 5), (2, 8)]
-    print(topological_sort(relations))
+    #print(topological_sort(relations))
+    zero = [(0, (0, 0, -1))]
+    p = [(1, (1, 0, 0)), (1, (0, 1, 0)), (1, (0, 0, 1))] + zero
+    print(polynomial_addition(p, zero))
--- python/tests.py
+++ python/tests.py
@@ -1,7 +1,7 @@
 import unittest
 from permutations import table_to_cycles, cycles_to_table
 from permutations import permutation_product_A, permutation_product_B, permutation_inverse_I
-from linkedlists import topological_sort
+from linkedlists import topological_sort, polynomial_addition
 
 class PermutationTest(unittest.TestCase):
 
@@ -58,6 +58,11 @@ class LinkedlistsTest(unittest.TestCase):
         self.knuth = [(9, 2), (3, 7), (7, 5), (5, 8), (8, 6), (4, 6), (1, 3), (7, 4), (9, 5), (2, 8)]
         self.knuth_res = [9, 2, 1, 3, 7, 5, 8, 4, 6]
         self.loop = [(1, 2), (2, 3), (3, 1), (1, 4)]
+        self.zero = [(0, (0, 0, -1))]
+        self.p = [(1, (1, 0, 0)), (1, (0, 1, 0)), (1, (0, 0, 1))] + self.zero
+        self.minus_p = [(-1, (1, 0, 0)), (-1, (0, 1, 0)), (-1, (0, 0, 1))] + self.zero
+        self.q = [(1, (2, 0, 0)), (-2, (0, 1, 0)), (-1, (0, 0, 1))] + self.zero
+        self.p_plus_q = [(1, (2, 0, 0)), (1, (1, 0, 0)), (-1, (0, 1, 0))] + self.zero
 
     def test_topological_sort(self):
         self.assertEqual(topological_sort(self.simple), [1, 2, 3, 4, 5])
@@ -65,3 +70,17 @@ class LinkedlistsTest(unittest.TestCase):
 
     def test_topological_sort_loop(self):
         self.assertRaises(ValueError, topological_sort, self.loop)
+
+    def test_polynomial_addition_zero(self):
+        self.assertEqual(polynomial_addition(self.zero, self.zero), self.zero)
+        self.assertEqual(polynomial_addition(self.p, self.zero), self.p)
+        self.assertEqual(polynomial_addition(self.zero, self.p), self.p)
+
+    def test_polynomial_addition(self):
+        self.assertEqual(polynomial_addition(self.p, self.q), self.p_plus_q)
+        self.assertEqual(polynomial_addition(self.p, self.minus_p), self.zero)
+        self.assertEqual(polynomial_addition(self.p_plus_q, self.minus_p), self.q)
+
+    def test_polynomial_addition_commut(self):
+        self.assertEqual(polynomial_addition(self.q, self.p), self.p_plus_q)
+        self.assertEqual(polynomial_addition(self.minus_p, self.p), self.zero)
